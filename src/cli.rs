8bd4c280509b8fc42d789ac924e5a701095d7b2d
Natan <natanvnascimento@gmail.com>
2026-02-09 19:22:02 -0300

    Set CLI name to "kuken"

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,7 @@ use crate::commands;
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(version, about, long_about = None, name = "kuken")]
 pub struct Cli {
     #[command(subcommand)]
     pub(crate) command: commands::Commands,
